91350efefe801e988cea695dcaa9edecf3c15c25
kgneethu <neethukg@gmail.com>
2017-12-02 12:02:30 +0000

    Corrected the BiConsumer Argument order

--- src/main/java/com/fractallabs/assignment/TwitterScanner.java
+++ src/main/java/com/fractallabs/assignment/TwitterScanner.java
@@ -19,7 +19,7 @@ public class TwitterScanner {
     final int[] sum = new int[1];
     List<TSValue> tsValuesList = new ArrayList<>();
 
-   public static class TSValue {
+    public static class TSValue {
         private final Instant timestamp;
         private final double val;
 
@@ -27,6 +27,7 @@ public class TwitterScanner {
             this.timestamp = timestamp;
             this.val = val;
         }
+
         public Instant getTimestamp() {
             return timestamp;
         }
@@ -38,6 +39,7 @@ public class TwitterScanner {
 
     public TwitterScanner(String companyName) {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
         configurationBuilder.setDebugEnabled(true)
                 .setOAuthConsumerKey("b3SNw4VosXmqGpgiN6C5jBc2R")
                 .setOAuthConsumerSecret("STSFUecoYK9XXSk6ucjqaSFCQz5F0KHzOjpyXJH8uQscuviA67")
@@ -84,30 +86,38 @@ public class TwitterScanner {
         tsValuesList.add(value);
         int index = tsValuesList.size();
         if(tsValuesList.size() > 1) {
-            System.out.println("Old mentions>>>>>" + tsValuesList.get(index - 2).val + "-----" + tsValuesList.get(index - 2).timestamp);
-            System.out.println("New mentions>>>>>" + tsValuesList.get(index - 1).val + "-----" + tsValuesList.get(index - 1).timestamp);
-            if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val && tsValuesList.get(index - 2).val == 0)  System.out.println("Initial increase....");
-            else if(tsValuesList.get(index - 1).val == tsValuesList.get(index - 2).val) System.out.println("No change!!!");
-            else if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val) performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
-            else performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
+            System.out.println("Old mentions >>>> " + tsValuesList.get(index - 2).val + " --------- " + tsValuesList.get(index - 2).timestamp);
+            System.out.println("New mentions >>>> " + tsValuesList.get(index - 1).val + " --------- " + tsValuesList.get(index - 1).timestamp);
+
+            if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val && tsValuesList.get(index - 2).val == 0) {
+                System.out.println("Initial increase....");
+            }
+            else if(tsValuesList.get(index - 1).val == tsValuesList.get(index - 2).val) {
+                System.out.println("No change!!!");
+            }
+            else if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val) {
+                performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
+            }
+            else {
+                performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
+            }
         }
     }
 
     BiFunction<Double, Double, Double> difference = (value1, value2) -> (value1 > value2) ? value1 - value2 : value2 - value1;
     Function<Double, String> convertDecimal = value -> new DecimalFormat("#.00").format(value).toString() + "%";
     BiFunction<Double, Double, Double> percentageCal = (value, original) -> value * 100 / original;
-    BiFunction<Double, Double, String> status = (value1, value2) -> (value1 > value2) ? "Increased by: " : "Decreased by: ";
+    BiFunction<Double, Double, String> status = (value1, value2) -> (value1 > value2) ? "Increased by --- " : "Decreased by --- ";
     BiConsumer<Double, Double> performAction = this::accept;
 
-    private void accept(Double originalMention, Double newMention) {
+    private void accept(Double newMention, Double originalMention) {
         System.out.println(
-                status.apply(originalMention, newMention) +
+                status.apply(newMention, originalMention) +
                         convertDecimal.apply(
                                 percentageCal.apply(
                                         difference.apply(newMention, originalMention),
                                         originalMention)
-                        )
-        );
+                        ));
     }
 
     public static void main(String ... args) {
