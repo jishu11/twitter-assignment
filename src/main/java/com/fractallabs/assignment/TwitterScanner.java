b47743a77f36a2d5d2dd1dae9df44b04c0005084
kgneethu <neethukg@gmail.com>
2017-12-02 11:42:48 +0000

    Introduce Biconsumer to clean up the code

--- src/main/java/com/fractallabs/assignment/TwitterScanner.java
+++ src/main/java/com/fractallabs/assignment/TwitterScanner.java
@@ -19,9 +19,7 @@ public class TwitterScanner {
     final int[] sum = new int[1];
     List<TSValue> tsValuesList = new ArrayList<>();
 
-    public TwitterScanner() { }
-
-    public static class TSValue {
+   public static class TSValue {
         private final Instant timestamp;
         private final double val;
 
@@ -29,7 +27,6 @@ public class TwitterScanner {
             this.timestamp = timestamp;
             this.val = val;
         }
-
         public Instant getTimestamp() {
             return timestamp;
         }
@@ -41,7 +38,6 @@ public class TwitterScanner {
 
     public TwitterScanner(String companyName) {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-
         configurationBuilder.setDebugEnabled(true)
                 .setOAuthConsumerKey("b3SNw4VosXmqGpgiN6C5jBc2R")
                 .setOAuthConsumerSecret("STSFUecoYK9XXSk6ucjqaSFCQz5F0KHzOjpyXJH8uQscuviA67")
@@ -81,39 +77,41 @@ public class TwitterScanner {
             TwitterScanner.TSValue tsValue = new TwitterScanner.TSValue(java.time.Instant.now(), Double.valueOf(sum[0]));
             storeValue(tsValue);
             sum[0] = 0;
-        }, 0, 5, TimeUnit.SECONDS);
+        }, 0, 1, TimeUnit.HOURS);
     }
 
     private void storeValue(TSValue value) {
         tsValuesList.add(value);
         int index = tsValuesList.size();
         if(tsValuesList.size() > 1) {
-            System.out.println("Old mentions: " + tsValuesList.get(index - 2).val + "-----" + tsValuesList.get(index - 2).timestamp);
-            System.out.println("New mentions: " + tsValuesList.get(index - 1).val + "-----" + tsValuesList.get(index - 1).timestamp);
-
-            if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val && tsValuesList.get(index - 2).val == 0) {
-                System.out.println("Initial increase....");
-            }
-            else if(tsValuesList.get(index - 1).val == tsValuesList.get(index - 2).val) {
-                System.out.println("No change!!!");
-            }
-            else if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val) {
-                System.out.println("Increased by " + convertDecimal.apply(percentageCal.apply
-                        (difference.apply(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val), tsValuesList.get(index - 2 ).val)));
-            }
-            else {
-                System.out.println("Decreased by " + convertDecimal.apply(percentageCal.apply
-                        (difference.apply(tsValuesList.get(index - 2).val, tsValuesList.get(index - 1).val), tsValuesList.get(index - 2 ).val)));
-            }
+            System.out.println("Old mentions>>>>>" + tsValuesList.get(index - 2).val + "-----" + tsValuesList.get(index - 2).timestamp);
+            System.out.println("New mentions>>>>>" + tsValuesList.get(index - 1).val + "-----" + tsValuesList.get(index - 1).timestamp);
+            if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val && tsValuesList.get(index - 2).val == 0)  System.out.println("Initial increase....");
+            else if(tsValuesList.get(index - 1).val == tsValuesList.get(index - 2).val) System.out.println("No change!!!");
+            else if(tsValuesList.get(index - 1).val > tsValuesList.get(index - 2).val) performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
+            else performAction.accept(tsValuesList.get(index - 1).val, tsValuesList.get(index - 2).val);
         }
     }
 
-    BiFunction<Double, Double, Double> difference = (value1, value2) -> value1 - value2;
+    BiFunction<Double, Double, Double> difference = (value1, value2) -> (value1 > value2) ? value1 - value2 : value2 - value1;
     Function<Double, String> convertDecimal = value -> new DecimalFormat("#.00").format(value).toString() + "%";
     BiFunction<Double, Double, Double> percentageCal = (value, original) -> value * 100 / original;
+    BiFunction<Double, Double, String> status = (value1, value2) -> (value1 > value2) ? "Increased by: " : "Decreased by: ";
+    BiConsumer<Double, Double> performAction = this::accept;
+
+    private void accept(Double originalMention, Double newMention) {
+        System.out.println(
+                status.apply(originalMention, newMention) +
+                        convertDecimal.apply(
+                                percentageCal.apply(
+                                        difference.apply(newMention, originalMention),
+                                        originalMention)
+                        )
+        );
+    }
 
     public static void main(String ... args) {
-        TwitterScanner scanner = new TwitterScanner("London");
+        TwitterScanner scanner = new TwitterScanner("Facebook");
         scanner.run();
     }
 
